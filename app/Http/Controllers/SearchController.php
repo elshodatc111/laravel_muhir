fb4b77cf86fd820091676779f9c08c279a39a805
USER <elshodatc111>
2024-10-07 15:25:42 +0500

    Save

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -1,15 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Muxir;
+use App\Models\Bolim;
+use App\Models\Hodim;
+use App\Models\MuxirFaktura;
+use App\Models\NaryadB;
+use App\Models\NaryadBlankaFaktura;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller{
     public function qidruv_muxir(){
-        return "qidruv_muxir";
+        $Muxir = Muxir::where('muxirs.status','true')
+            ->join('muxir_fakturas','muxir_fakturas.number','=','muxirs.faktura')
+            ->select('muxirs.number','muxirs.faktura','muxir_fakturas.coato','muxir_fakturas.coato_name','muxir_fakturas.hodim','muxir_fakturas.operator','muxir_fakturas.created_at')
+            ->get();
+        return view('search.muxir',compact('Muxir'));
     }
     public function qidruv_naryad_blanka(){
-        return "qidruv_naryad_blanka";
+        $Muxir = NaryadB::where('naryad_b_s.status','true')
+            ->join('naryad_blanka_fakturas','naryad_blanka_fakturas.number','=','naryad_b_s.faktura')
+            ->select('naryad_b_s.number','naryad_b_s.faktura','naryad_blanka_fakturas.coato','naryad_blanka_fakturas.coato_name','naryad_blanka_fakturas.hodim','naryad_blanka_fakturas.operator','naryad_blanka_fakturas.created_at')
+            ->get();
+        return view('search.naryad_blanka',compact('Muxir'));
     }
     public function qidruv_simkarta(){
         return "qidruv_simkarta";
---
+++ resources/views/search/naryad_blanka.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.layout')
+@section('title',"Home")
+@section('content')
+<main id="main" class="main">
+
+<div class="pagetitle">
+  <h1>Naryadlar</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="{{ route('home') }}">Muhir</a></li>
+      <li class="breadcrumb-item active">Naryadlar(Blanka)</li>
+    </ol>
+  </nav>
+</div>
+@if (Session::has('success'))
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <i class="bi bi-check-circle me-1"></i>
+    {{Session::get('success') }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+@elseif (Session::has('error'))
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <i class="bi bi-check-circle me-1"></i>
+    {{Session::get('error') }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+@endif
+<div class="row mb-3">
+  <div class="col-3"><a href="{{ route('qidruv_muxir') }}" class="btn btn-secondary w-100">Muxirlar</a></div>
+  <div class="col-3"><a href="{{ ROUTE('qidruv_naryad_blanka') }}" class="btn btn-primary w-100">Naryad (blanka)</a></div>
+  <div class="col-3"><a href="{{ ROUTE('qidruv_simkarta') }}" class="btn btn-secondary w-100">Sim kartalar</a></div>
+  <div class="col-3"><a href="{{ ROUTE('qidruv_naryadlar') }}" class="btn btn-secondary w-100">Naryadlar</a></div>
+</div>
+<section class="section dashboard">
+  <div class="card recent-sales overflow-auto">
+    <div class="card-body">   
+      <table class="table text-center table-striped table-bordered datatable">
+        <thead>
+          <tr>
+            <th class="bg-primary text-white text-center">#</th>
+            <th class="bg-primary text-white text-center">Naryad raqami</th>
+            <th class="bg-primary text-white text-center">Hisob faktura raqami</th>
+            <th class="bg-primary text-white text-center">Bo'lim</th>
+            <th class="bg-primary text-white text-center">Masul hodim</th>
+            <th class="bg-primary text-white text-center">Operator</th>
+            <th class="bg-primary text-white text-center">Tarqatildi</th>
+          </tr>
+        </thead>
+        <tbody>
+          @forelse($Muxir as $item)
+          <tr>
+            <td>{{$loop->index+1}}</td>
+            <td>{{ $item['number'] }}</td>
+            <td><a href="{{ route('muxir_faktura_show',$item['faktura']) }}"> № - {{ $item['faktura'] }}</a></td>
+            <td>{{ $item['coato_name'] }}</td>
+            <td>{{ $item['hodim'] }}</td>
+            <td>{{ $item['operator'] }}</td>
+            <td>{{ $item['created_at'] }}</td>
+          </tr>
+          @empty
+
+          @endforelse
+        </tbody>
+      </table>
+    </div>
+  </div>
+</section>
+
+</main>
+@endsection
